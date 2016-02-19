c0e5580d62bb545b3a77d684fc43df1e2a4e7e61
Nick Herrera <nherrera@editllc.com>
2016-02-19 14:43:04 -0800

    chore(build): refactor `getStyleStream` into a generic utility function called `createOrderedStream`

--- gulpfile.js
+++ gulpfile.js
@@ -10,25 +10,15 @@ var buildPath = 'build/';
 gulp.task('dev:styles', compileStyles);
 
 function compileStyles() {
-	return file('montage.scss', '/* inject:scss */\n/* endinject */', { src: true })
-		.pipe(inject(getStyleStream(), { relative: true }))
-		.pipe(sass())
-		.pipe(gulp.dest(buildPath));
-}
 
-/**
- * Create a stream of SCSS files that preserves ITCSS ordering.
- */
-function getStyleStream() {
+	// Create a stream of SCSS files that preserves ITCSS ordering
 	var sources = [
-		'src/assets/styles/*.settings.scss',
-		'src/assets/styles/*.tools.scss',
-		'src/assets/styles/*.generic.scss',
-		'src/assets/styles/*.base.scss',
-		'src/assets/styles/*.object.scss',
-
-		// Catch any SCSS files that do not follow the ITCSS naming convention.
-		[
+		['src/assets/styles/*.settings.scss'],
+		['src/assets/styles/*.tools.scss'],
+		['src/assets/styles/*.generic.scss'],
+		['src/assets/styles/*.base.scss'],
+		['src/assets/styles/*.object.scss'],
+		[ // Catch any SCSS files that do not follow the ITCSS naming convention
 			'src/assets/styles/*.scss',
 			'!src/assets/styles/*.settings.scss',
 			'!src/assets/styles/*.tools.scss',
@@ -36,12 +26,26 @@ function getStyleStream() {
 			'!src/assets/styles/*.base.scss',
 			'!src/assets/styles/*.object.scss'
 		],
-		'src/views/**/*.scss',
-		'src/assets/styles/*.trump.scss'
+		['src/views/**/*.scss'],
+		['src/assets/styles/*.trump.scss']
 	];
 
-	var streams = sources.map(function(source) {
-		return gulp.src(source, { read: false }).pipe(naturalSort());
+	return file('montage.scss', '/* inject:scss */\n/* endinject */', { src: true })
+		.pipe(inject(createOrderedStream(sources, { read: false }), { relative: true }))
+		.pipe(sass())
+		.pipe(gulp.dest(buildPath));
+}
+
+/**
+ * Combine an array of sources into a single source stream, while preserving the original order.
+ *
+ * @param {string[]|string[][]} globGroups - An array of source globs to concatenate
+ * @param {Object} [options] - Options to pass to `gulp.src`
+ * @returns {Stream}
+ */
+function createOrderedStream(globGroups, options) {
+	var streams = globGroups.map(function(globs) {
+		return gulp.src(globs, options).pipe(naturalSort());
 	});
 
 	return streamSeries.apply(null, streams);
