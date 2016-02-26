fe1f7704f0811569a05a4f586f9e280c157409e0
Nick Herrera <nherrera@editllc.com>
2016-02-25 18:18:59 -0800

    feat(login): allow users to log into the system

--- src/views/login/login.component.js
+++ src/views/login/login.component.js
@@ -9,8 +9,23 @@
 			controller: loginController
 		});
 
-	function loginController() {
+	function loginController($state, userService) {
 		var vm = this;
 
+		vm.login = function(credentials) {
+			userService
+				.login(credentials)
+				.then(()=> $state.go('dashboard'))
+				.catch((error) => {
+					switch(error.status) {
+						case 401:
+							return vm.errorMessage = "Invalid credentials. Please try again.";
+						case 405:
+							return vm.errorMessage = "Invalid domain. Please try again.";
+						default:
+							return vm.errorMessage = "An error occurred. Please try again.";
+					}
+				});
+		};
 	}
 })(angular);
