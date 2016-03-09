eb376e4d3112053b20ff448e3c512ac2926b9e04
Nick Herrera <nherrera@editllc.com>
2016-03-08 17:29:34 -0800

    feat(record-browser): get a list of documents when the query changes

--- src/views/record-browser/record-browser.component.js
+++ src/views/record-browser/record-browser.component.js
@@ -14,5 +14,15 @@
 
 		api.schema.list().then(schemaList => vm.schemaList = schemaList);
 
+		vm.executeQuery = function(query) {
+			var schema = vm.schemaList.filter(schema => query.schema === schema.name)[0];
+
+			if(!schema) {
+				throw new Error("Schema not found.");
+			}
+
+			api.document.list(query.schema)
+				.then(documentList => { vm.results = { schema: schema.fields, documentList }});
+		};
 	}
 })(angular);
