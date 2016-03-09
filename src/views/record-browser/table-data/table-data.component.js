bfe157387b10d75c00e38351f65b979f1d2ef4de
Nick Herrera <nherrera@editllc.com>
2016-03-09 08:47:44 -0800

    feat(table-data): display documents in a table

--- src/views/record-browser/table-data/table-data.component.js
+++ src/views/record-browser/table-data/table-data.component.js
@@ -6,12 +6,67 @@
 		.component('tableData', {
 			templateUrl: 'views/record-browser/table-data/table-data.html',
 			controllerAs: 'tableData',
-			controller: tableDataController
+			controller: tableDataController,
+			bindings: {
+				schemaFields: '=',
+				data: '='
+			}
 		});
 
-	function tableDataController() {
+	function tableDataController($scope) {
 		var vm = this;
+		vm.columns = [];
+		vm.rows = [];
 
+		$scope.$watch(() => vm.data, formatData);
+
+		function formatData() {
+			if(vm.data && vm.schemaFields) {
+				vm.columns = getColumns(vm.schemaFields, vm.data);
+				vm.rows = getRows(vm.columns, vm.data);
+			}
+		}
+
+		function getColumns(schema, data) {
+			var columns = [];
+			var index = 0;
+
+			// Add schema columns to the columnDictionary
+			var columnDictionary = schema.reduce(function(columnDictionary, currentColumn) {
+				if(!columnDictionary[currentColumn.name]) {
+					currentColumn.index = index++;
+					columnDictionary[currentColumn.name] = currentColumn;
+				}
+
+				return columnDictionary;
+			}, {});
+
+			// Add non-schema columns to the columnDictionary
+			data.forEach(document => {
+				Object.keys(document).forEach(key => {
+					if(!columnDictionary[key] && key !== '_meta') {
+						columnDictionary[key] = {
+							index: index++,
+							name: key,
+							nonSchema: true
+						};
+					}
+				});
+			});
+
+			// Convert the columnDictionary into an array
+			for(var key in columnDictionary) {
+				columns[columnDictionary[key].index] = columnDictionary[key];
+			}
+
+			return columns;
+		}
+
+		function getRows(columns, data) {
+
+			// Convert document objects into arrays with the same order as `columns`
+			return data.map(document => columns.map(column => document[column.name]));
+		}
 
 	}
 })(angular);
