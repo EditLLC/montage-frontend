8b67e2c6a79c4eafd581bf850f4613046b03b056
Nick Herrera <nherrera@editllc.com>
2016-03-31 12:34:51 -0700

    chore(build): create a `compile-sass` task to compile Sass and copy to the build directory

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,7 +1,10 @@
 var gulp        = require('gulp');
+var gutil       = require('gulp-util');
 
+var browserSync = require('browser-sync');
 var inject      = require('gulp-inject');
 var naturalSort = require('gulp-natural-sort');
+var sass        = require('gulp-sass');
 
 var path        = require('../paths.js');
 
@@ -14,3 +17,17 @@ gulp.task('inject-sass', function() {
 		.pipe(inject(sources, { relative: true }))
 		.pipe(gulp.dest(path.to.sass.main.destination));
 });
+
+gulp.task('compile-sass', ['inject-sass'], function() {
+	return gulp.src(path.to.sass.main.source)
+		.pipe(sass())
+		.on('error', handleError)
+		.pipe(gulp.dest(path.to.sass.destination))
+		.pipe(browserSync.stream()); // Inject styles into the browser when serving files though `browserSync`
+});
+
+function handleError(error) {
+	gutil.beep();
+	console.log('\n', error.formatted, '\n');
+	this.emit('end');
+}
