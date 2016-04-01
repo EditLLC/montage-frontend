bf866df7c0f4dd9c153cc9533509f7044aab0b8c
Nick Herrera <nherrera@editllc.com>
2016-04-01 14:13:05 -0700

    chore(build): create a `develop` task to build the un-minified version of the app

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
 var gulp        = require('gulp');
 var requireDir  = require('require-dir');
+var runSequence = require('run-sequence');
 
 // Require all tasks
 requireDir('./gulp/tasks', { recurse: true });
+
+
+gulp.task('develop', function() {
+	runSequence(
+		'clean',
+		'compile-sass',
+		'compile-scripts',
+		'inject-libs',
+		'copy-libs',
+		'copy-all'
+	);
+});
