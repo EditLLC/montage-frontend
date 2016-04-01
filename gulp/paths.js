c6eb994058d2be852001fece2bbe6490de28779f
Nick Herrera <nherrera@editllc.com>
2016-03-31 23:12:09 -0700

    chore(build): place asset files in the `assets/` directory

--- gulp/paths.js
+++ gulp/paths.js
@@ -8,7 +8,7 @@ module.exports = {
 
 		assets: {
 			source: ['src/assets/**/*', '!src/assets/styles{,/**}'],
-			destination: destination
+			destination: destination + 'assets/'
 		},
 
 		templates: {
