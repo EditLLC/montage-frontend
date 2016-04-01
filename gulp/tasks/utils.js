59370b2f3e5ea4d0e7604a11f000591b587b9970
Nick Herrera <nherrera@editllc.com>
2016-03-31 22:16:50 -0700

    chore(build): add a `serve` task to serve the build directory through a `browser-sync` web server

--- gulp/tasks/utils.js
+++ gulp/tasks/utils.js
@@ -1,9 +1,27 @@
 var gulp = require('gulp');
-var del  = require('del');
+
+var del = require('del');
+var browserSync = require('browser-sync');
+var historyApiFallback = require('connect-history-api-fallback');
 
 var path = require('../paths.js');
 
 
 gulp.task('clean', function() {
 	return del(path.to.destination);
+});
+
+gulp.task('serve', function() {
+	browserSync.init({
+		open: false,
+		server: {
+			baseDir: path.to.destination,
+			middleware: [historyApiFallback()],
+			ghostMode: {
+				clicks: true,
+				forms: true,
+				scroll: true
+			}
+		}
+	});
 });
