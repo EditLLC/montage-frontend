93f3411830169ae6f79d0984282e2cb77d5cb721
Nick Herrera <nherrera@editllc.com>
2016-04-01 14:04:56 -0700

    chore(build): add a `compile-templates` task to wrap templates with `$templateCache` and concatenate into to one file

--- gulp/tasks/assets.js
+++ gulp/tasks/assets.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 
+var htmlmin = require('gulp-htmlmin');
+var templateCache = require('gulp-angular-templatecache');
+
 var path = require('../paths.js');
 
 gulp.task('copy-index', function() {
@@ -10,6 +13,24 @@ gulp.task('copy-index', function() {
 gulp.task('copy-templates', function() {
 	return gulp.src(path.to.templates.source)
 		.pipe(gulp.dest(path.to.templates.destination));
+});
+
+gulp.task('compile-templates', ['copy-templates'], function() {
+	var htmlminOptions = {
+		removeComments: true,
+		collapseWhitespace: true,
+		conservativeCollapse: true,
+		collapseBooleanAttributes: true,
+		removeEmptyAttributes: true,
+		removeScriptTypeAttributes: true,
+		minifyJS: true,
+		minifyCSS: true
+	};
+
+	return gulp.src(path.to.template.destination + '**/*.html')
+		.pipe(htmlmin(htmlminOptions))
+		.pipe(templateCache({ module: 'montage', root: 'templates/' }))
+		.pipe(gulp.dest(path.to.destination));
 });
 
 gulp.task('copy-assets', function() {
