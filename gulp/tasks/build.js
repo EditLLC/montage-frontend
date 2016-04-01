a28fb9547e550645522b9f34a9434f857b66c9bb
Nick Herrera <nherrera@editllc.com>
2016-04-01 14:41:36 -0700

    chore(build): create a `make-build` task to minify sources

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,4 +1,34 @@
 var gulp = require('gulp');
 
+var cssmin     = require('gulp-cssmin');
+var gulpif     = require('gulp-if');
+var htmlmin    = require('gulp-htmlmin');
+var ngAnnotate = require('gulp-ng-annotate');
+var uglify     = require('gulp-uglify');
+var useref     = require('gulp-useref');
+
+var path = require('../paths.js');
+
 
 gulp.task('pre-build', ['compile-scripts', 'copy-libs', 'compile-sass', 'copy-all', 'inject-templates']);
+
+gulp.task('make-build', function() {
+	var htmlminOptions = {
+		removeComments: true,
+		collapseWhitespace: true,
+		conservativeCollapse: true,
+		collapseBooleanAttributes: true,
+		removeEmptyAttributes: true,
+		removeScriptTypeAttributes: true,
+		minifyJS: true,
+		minifyCSS: true
+	};
+
+	return gulp.src(path.to.destination + 'index.html')
+		.pipe(useref({ searchPath: [path.to.destination] }))
+		.pipe(gulpif('app.js', ngAnnotate()))
+		.pipe(gulpif('*.js', uglify()))
+		.pipe(gulpif('*.css', cssmin({ keepSpecialComments: 0 })))
+		.pipe(gulpif('*.html', htmlmin(htmlminOptions)))
+		.pipe(gulp.dest(path.to.destination));
+});
