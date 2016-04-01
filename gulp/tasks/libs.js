54780bf5c0100c503ce7fd3cf2713c109bff2f6d
Nick Herrera <nherrera@editllc.com>
2016-03-31 22:11:28 -0700

    chore(build): add an `inject-libs` task to inject bower scripts and styles into `index.html`

---
+++ gulp/tasks/libs.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var wiredep = require('wiredep');
+
+var path = require('../paths.js');
+
+gulp.task('inject-libs', function() {
+	var wiredepOptions = {
+		ignorePath: '../bower_components/',
+		fileTypes: {
+			html: {
+				block: /(([ \t]*)<!--\s*build:*(\S*)\slib\.(?:css|js)\s*-->)(\n|\r|.)*?(<!--\s*endbuild\s*-->)/gi,
+				replace: {
+					js: '<script src="assets/libs/{{filePath}}"></script>',
+					css: '<link rel="stylesheet" href="assets/libs/{{filePath}}" />'
+				}
+			}
+		}
+	};
+
+	return gulp.src(path.to.index.source)
+		.pipe(wiredep.stream(wiredepOptions))
+		.pipe(gulp.dest(path.to.index.destination));
+});
