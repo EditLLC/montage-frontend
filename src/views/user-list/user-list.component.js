1f907e8869dc2d25deec09f7ecb36b19e7dc065a
Ryeker J Herndon <ryekerjherndon@Ryekers-MacBook-Pro.local>
2016-04-12 13:06:05 -0700

    refactor: change user service and component to meet dynamic generation.

--- src/views/user-list/user-list.component.js
+++ src/views/user-list/user-list.component.js
@@ -9,14 +9,12 @@
 			controller: userListController
 		});
 
-	function userListController(api) {
+	function userListController(api, userService) {
 		var vm = this;
 
-		api.user.list().then(userList => vm.userList = userList);
-		
-		// TODO: implement
-		vm.deleteUser = function(user_id) {
-			console.log('deleteUser() has not yet been implemented'); // TODO: REMOVE ME
-		};
+		api.user.list()
+			.then(userList => vm.userList = userList)
+			.then(userList => console.log('userList', userList));
+
 	}
 })(angular);
