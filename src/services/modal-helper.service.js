7961ae0dbd4fdd776b5bbd953fd0df6ca03c8b3a
Nick Herrera <nherrera@editllc.com>
2016-04-13 16:48:10 -0700

    feat(modal-helper): create a generic delete-confirmation modal

---
+++ src/services/modal-helper.service.js
@@ -0,0 +1,26 @@
+(function (angular) {
+	'use strict';
+
+	angular
+		.module('montage')
+		.factory('modalHelper', modalHelper);
+
+	function modalHelper($mdDialog) {
+		return {
+			confirmDelete
+		};
+
+		////////////
+
+		function confirmDelete(recordType = 'item') {
+			var confirm = $mdDialog.confirm()
+				.title('Delete Confirmation')
+				.textContent(`Are you sure you want to delete this ${recordType}?`)
+				.ariaLabel('Delete confirmation')
+				.ok('Delete')
+				.cancel('Cancel');
+
+			return $mdDialog.show(confirm);
+		}
+	}
+})(angular);
