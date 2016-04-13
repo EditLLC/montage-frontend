aa694b5938913dca47b2412cd5bc621e1a4dcc4b
Nick Herrera <nherrera@editllc.com>
2016-04-13 10:43:54 -0700

    feat(file-browser): implement file uploads

--- src/views/file-browser/file-browser.component.js
+++ src/views/file-browser/file-browser.component.js
@@ -9,14 +9,36 @@
 			controller: fileBrowserController
 		});
 
-	function fileBrowserController(api) {
+	function fileBrowserController($scope, api) {
 		var vm = this;
+
+		vm.totalCount = vm.pendingCount = vm.doneCount = 0;
 
 		api.file.getFileList()
 			.then(fileList => vm.fileList = fileList);
 
 		vm.toggleDropZone = function() {
 			vm.isDropZoneVisible = !vm.isDropZoneVisible;
+			vm.totalCount = vm.pendingCount = vm.doneCount = 0;
 		};
+
+		vm.upload = files => {
+			vm.pendingCount += files.length;
+			vm.totalCount += files.length;
+
+			files.forEach(file => {
+				api.file.uploadFile(file)
+					.then(fileInfo => {
+						vm.doneCount++;
+						vm.pendingCount--;
+					});
+			});
+		};
+		
+		$scope.$watch(() => vm.pendingFiles, (pendingFiles) => {
+			if(pendingFiles) {
+				vm.upload(pendingFiles);
+			}
+		});
 	}
 })(angular);
