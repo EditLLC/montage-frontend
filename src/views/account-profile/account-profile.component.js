097e2e602f35211356f130d1efc462b7c6398fa3
Nick Herrera <nherrera@editllc.com>
2016-04-14 00:33:34 -0700

    feat(account-profile): implement profile updates

--- src/views/account-profile/account-profile.component.js
+++ src/views/account-profile/account-profile.component.js
@@ -9,14 +9,24 @@
 			controller: accountProfileController
 		});
 
-	function accountProfileController(authService) {
+	function accountProfileController(authService, api) {
 		var vm = this;
 
-		vm.currentUser = authService.getCurrentUser();
+		// NOTE: Use `angular.copy()` to prevent the user's name from changing in the nav bar
+		vm.currentUser = angular.copy(authService.getCurrentUser());
 
-		// TODO: implement
-		vm.save = function(user) {
-			console.log('Saving account profiles is not implemented');
+		vm.save = function({id, full_name, email}) {
+			vm.isSaving = true;
+
+			api.user.update(id, full_name, email)
+				.then(user => {
+					vm.status = 'success';
+
+					vm.currentUser = user;
+					authService.setCurrentUser(user)
+				})
+				.catch(() => vm.status = 'error')
+				.finally(() => vm.isSaving = false);
 		}
 	}
 })(angular);
