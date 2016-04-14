1e8c2b89cf1fd2fec39782c938ca93faed404624
Ryeker J Herndon <ryekerjherndon@Ryekers-MacBook-Pro.local>
2016-04-14 12:58:56 -0700

    refactor(user):
    refactor delete user and view, add confirm modal

--- src/views/user-list/user-list.component.js
+++ src/views/user-list/user-list.component.js
@@ -9,17 +9,15 @@
 			controller: userListController
 		});
 
-	function userListController(api) {
+	function userListController(api, modalHelper) {
 		var vm = this;
 
 		api.user.list()
 			.then(userList => vm.userList = userList);
 
-		vm.removeUser = function (user_id) {
-			console.log(user_id);
-			api.user.remove(user_id)
-			.then ((user) => console.log(user.id));
-			//TODO add a catch for failure of this function and a modal to confirm
+		vm.confirmRemove = function (id) {
+			modalHelper.confirmDelete(id)
+				.then(() => api.user.remove(id))
 		};
 	}
 })(angular);
