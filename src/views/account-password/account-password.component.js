c21a327c2c67110f55d320c5c7b8a6127ddbd4e1
Nick Herrera <nherrera@editllc.com>
2016-04-14 01:03:24 -0700

    feat(account-password): allow users to change their passwords

--- src/views/account-password/account-password.component.js
+++ src/views/account-password/account-password.component.js
@@ -9,12 +9,27 @@
 			controller: accountPasswordController
 		});
 
-	function accountPasswordController() {
+	function accountPasswordController(authService, api) {
 		var vm = this;
 
 		// TODO: implement
-		vm.changePassword = function(passwords) {
-			console.log('Changing passwords is not implemented');
+		vm.changePassword = function() {
+			vm.isSaving = true;
+
+			var currentUser = authService.getCurrentUser();
+
+			authService.login(currentUser.email, vm.currentPassword)
+				.then(() => api.user.update(currentUser.id, null, null, vm.newPassword))
+				.then(() => vm.status = 'success')
+				.catch(() => vm.status = 'error')
+				.finally(() => {
+					vm.isSaving = false
+
+					// Reset the form
+					vm.currentPassword = vm.newPassword = vm.confirmedPassword = '';
+					vm.changePasswordForm.$setPristine();
+					vm.changePasswordForm.$setUntouched();
+				});
 		}
 	}
 })(angular);
