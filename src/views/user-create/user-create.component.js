7a9f6a8d4f2d877d8a68fa56699c13999b9d5480
Ryeker J Herndon <ryekerjherndon@Ryekers-MacBook-Pro.local>
2016-04-18 14:53:32 -0700

    refactor(callback & state.go): change callback to a function and add state.go onClick

--- src/views/user-create/user-create.component.js
+++ src/views/user-create/user-create.component.js
@@ -5,17 +5,18 @@
 		.module('montage')
 		.component('userCreate', {
 			templateUrl: 'views/user-create/user-create.html',
-			controllerAs: 'userCreate',
+			controllerAs: 'createUser',
 			controller: userCreateController
 		});
 
-		function userCreateController(api, $state) {
-			var vm = this;
+	function userCreateController(api, $state) {
+		var vm = this;
 
-			vm.createUser = function (user_id, full_name, email, password) {
-				api.user.create(user_id, full_name, email, password)
-					.then((user) => vm.newUser = user)
-					.then((user) => $state.go('user.edit', ({ user_id: user.id })));
-				}
-			}
-	})(angular)
+		vm.createUser = function (full_name, email, password) {
+			api.user.create(full_name, email, password)
+				.then((user) => vm.newUser = user)
+				.then((user) => $state.go('user.edit', ({ user_id: user.id })));
+		}
+	}
+})(angular);
+
