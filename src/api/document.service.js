98c63c8f0e4b1d6e5b239d31373ff3767c0655cd
Julian Coleman <julcol03@gmail.com>
2016-07-21 12:18:24 -0700

    refactor(document): remove unused list method and dependencies

--- src/api/document.service.js
+++ src/api/document.service.js
@@ -5,42 +5,10 @@
 		.module('montage')
 		.factory('documentService', documentService);
 
-	function documentService($http, authService) {
-		var service = {
-			list
+	function documentService(montageHelper) {
+		return {
 		};
 
 		////////////
-
-		// TODO: reset these on login
-		var apiUri = `https://${authService.getCurrentUser().domain}.mntge.com/api/v1/schemas/`;
-		var authHeader = { Authorization: 'Token ' + authService.getCurrentUser().token };
-
-		var defaultQuery = {
-			filter: {},
-			pluck: [],
-			without: [],
-			limit: null,
-			offset: null,
-			order_by: null,
-			ordering: "asc",
-			index: null,
-			batch_size: 1000
-		};
-
-		function list(schema, query) {
-			var documentListUri = apiUri + schema + '/query/';
-
-			var config = {
-				headers: authHeader,
-				params: {
-					query: angular.extend({}, defaultQuery, query)
-				}
-			};
-
-			return $http.get(documentListUri, config).then(response => response.data.data);
-		}
-
-		return service;
 	}
 })(angular);
