65d9fccf69e51aff0ae4162e8401491ca552a7f0
Elanna Grossman <elanna.grossman@gmail.com>
2016-07-27 16:15:16 -0700

    feat(user-detail): check for current user was queried and add token if true

--- src/views/user-detail/user-detail.component.js
+++ src/views/user-detail/user-detail.component.js
@@ -12,6 +12,13 @@
 	function userDetailController($stateParams, authService, api) {
 		const vm = this;
 
-		api.user.get($stateParams.user_id).then(user => vm.user = user);
+		api.user.get($stateParams.user_id).then(user => {
+			vm.user = user;
+			const currentUser = authService.getCurrentUser();
+
+			if (currentUser.id === vm.user.id) {
+				vm.user.token = currentUser.token;
+			}
+		});
 	}
 })(angular);
