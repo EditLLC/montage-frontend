90472e1f7045e72a8a1855e2ab69e7cab62913d9
Elanna Grossman <elanna.grossman@gmail.com>
2016-08-10 21:41:38 -0700

    style(user-detail): indent block

--- src/views/user-detail/user-detail.component.js
+++ src/views/user-detail/user-detail.component.js
@@ -12,13 +12,14 @@
 	function UserDetailController($stateParams, authService, api) {
 		const vm = this;
 
-		api.user.get($stateParams.user_id).then(user => {
-			vm.user = user;
-			const currentUser = authService.getCurrentUser();
+		api.user.get($stateParams.user_id)
+			.then(user => {
+				vm.user = user;
+				const currentUser = authService.getCurrentUser();
 
-			if (currentUser.id === vm.user.id) {
-				vm.user.token = currentUser.token;
-			}
-		});
-	}
+				if (currentUser.id === vm.user.id) {
+					vm.user.token = currentUser.token;
+				}
+			});
+		}
 })(angular);
