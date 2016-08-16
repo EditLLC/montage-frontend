4dc8894ca7f0bea09e541b8a70880add5e429c64
Elanna Grossman <elanna.grossman@gmail.com>
2016-08-16 12:07:55 -0700

    refactor(user-form): simplify boolean passed to view

--- src/views/user-form/user-form.component.js
+++ src/views/user-form/user-form.component.js
@@ -68,11 +68,11 @@
 
 		function checkNotFound(error) {
 			if (error.status === 404) {
-				vm.isNotFound = true;
 				vm.params = {
 					param_id   : 'user id',
 					returnPage : 'user.list',
 					pageName   : 'Users',
+					isNotFound : true,
 				};
 
 				return vm.params;
