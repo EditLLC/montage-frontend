566ec3bc5b7c8985f263303f4d7f77ddaec63bbe
Elanna Grossman <elanna.grossman@gmail.com>
2016-08-16 15:21:19 -0700

    refactor(user-detail): abstract `checkIfCurrentUser` login into own function for clarity

--- src/views/user-detail/user-detail.component.js
+++ src/views/user-detail/user-detail.component.js
@@ -13,19 +13,21 @@
 		const vm = this;
 
 		api.user.get($stateParams.user_id)
-			.then(user => {
-				vm.user = user;
-				const currentUser = authService.getCurrentUser();
-
-				if (currentUser.id === vm.user.id) {
-					vm.user.token = currentUser.token;
-				}
-			})
+			.then(user => checkIfCurrentUser(user))
 			.then(() => vm.isFound = true)
 			.catch(error => {
 				if (notFoundHelper.checkNotFound(error)) {
 					vm.params = notFoundHelper.buildUserObject();
 				}
 			});
+
+		function checkIfCurrentUser(user) {
+			vm.user = user;
+			const currentUser = authService.getCurrentUser();
+
+			if (currentUser.id === vm.user.id) {
+				vm.user.token = currentUser.token;
+			}
+		}
 	}
 })(angular);
