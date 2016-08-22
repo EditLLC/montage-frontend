9be7a0b49dc53d9e5db202790dfcfdc4ded0e703
Elanna Grossman <elanna.grossman@gmail.com>
2016-08-22 15:07:26 -0700

    feat(role-form): inject `toast` service

--- src/views/role-form/role-form.component.js
+++ src/views/role-form/role-form.component.js
@@ -9,7 +9,7 @@
 			controller   : RoleFormController,
 		});
 
-	function RoleFormController($state, $q, $stateParams, api) {
+	function RoleFormController($state, $q, $stateParams, api, toast) {
 		const vm = this;
 
 		vm.formType = getFormType();
