1f6fe56484a12b96635989084b1810481c94e671
Nick Herrera <nherrera@editllc.com>
2016-08-23 10:32:02 -0700

    feat(Record): create a `Record` model

---
+++ src/models/record.model.js
@@ -0,0 +1,40 @@
+(function(angular) {
+
+	angular.module('montage')
+		.factory('Record', function(api, $q, utils, Field) {
+
+			class Record {
+				constructor(fields) {
+					this.fields = fields;
+				}
+
+				static get(schemaName, document_id) {
+					const recordPromise = api.document.get(schemaName, document_id)
+						.then(record => {
+							delete record._meta;
+							return record;
+						});
+
+					const schemaFieldsByNamePromise = api.schema.get(schemaName)
+						.then(schema => utils.makeDictionary(schema.fields, 'name'));
+
+					return $q.all([recordPromise, schemaFieldsByNamePromise])
+						.then(([record, schemaFieldsByName]) => {
+							const fieldNames = Object.keys(record);
+
+							const fields = fieldNames.map(fieldName => new Field({
+								name     : fieldName,
+								value    : record[fieldName],
+								datatype : schemaFieldsByName[fieldName].datatype,
+								required : schemaFieldsByName[fieldName].required,
+							}));
+
+							return new Record(fields);
+						});
+				}
+			}
+
+			return Record;
+		});
+
+})(angular);
