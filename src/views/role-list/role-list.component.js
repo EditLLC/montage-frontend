725e7c7b1bf965f3510959885b4bc62605d6648a
Elanna Grossman <elanna.grossman@gmail.com>
2016-08-30 10:42:50 -0700

    refactor(role-list): remove unused function

--- src/views/role-list/role-list.component.js
+++ src/views/role-list/role-list.component.js
@@ -13,27 +13,5 @@
 		const vm = this;
 
 		api.role.list().then(roleList => vm.roleList = roleList);
-
-		vm.delete = function(roleName) {
-			vm.isSaving = true;
-
-			modalHelper.confirmDelete('role')
-				.then(() => {
-					api.role.remove(roleName)
-						.then(() => removeRoleFromView(vm.roleList, roleName))
-						.then(() => vm.status = 'success')
-						.catch(() => vm.status = 'error')
-						.finally(() => vm.isSaving = false);
-				});
-		};
-
-		function removeRoleFromView(roleList, roleName) {
-			for (let index = 0; index < roleList.length; index++) {
-				if (roleList[index].name === roleName) {
-					roleList.splice(index, 1);
-					break;
-				}
-			}
-		}
 	}
 })(angular);
