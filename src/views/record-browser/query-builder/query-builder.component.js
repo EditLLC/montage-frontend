326f1910079110f7bdaa9f46cc52b505911b894c
Julian Coleman <julcol03@gmail.com>
2016-09-23 10:31:06 -0700

    align colons and add dangling commas for cleaner diffs

--- src/views/record-browser/query-builder/query-builder.component.js
+++ src/views/record-browser/query-builder/query-builder.component.js
@@ -4,12 +4,12 @@
 	angular
 		.module('montage')
 		.component('queryBuilder', {
-			templateUrl: 'views/record-browser/query-builder/query-builder.html',
-			controllerAs: 'queryBuilder',
-			controller: queryBuilderController,
-			bindings: {
-				onSubmit: '=',
-				schemaList: '='
+			templateUrl  : 'views/record-browser/query-builder/query-builder.html',
+			controllerAs : 'queryBuilder',
+			controller   : queryBuilderController,
+			bindings     : {
+				onSubmit   : '=',
+				schemaList : '=',
 			}
 		});
 
