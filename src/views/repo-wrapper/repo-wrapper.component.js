af99ba55e74eff1a4a3cb7512976854acad60e52
Elanna Grossman <elanna.grossman@gmail.com>
2016-10-10 13:09:41 -0700

    feat(repo-wrapper): add `repo-wrapper` stubs

---
+++ src/views/repo-wrapper/repo-wrapper.component.js
@@ -0,0 +1,15 @@
+(function (angular) {
+	'use strict';
+
+	angular
+		.module('montage')
+		.component('repoWrapper', {
+			templateUrl  : 'views/repo-wrapper/repo-wrapper.html',
+			controllerAs : 'repoWrapper',
+			controller   : RepoWrapperController,
+		});
+
+	function RepoWrapperController() {
+
+	}
+})(angular);
